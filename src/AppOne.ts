8a6555bcc2b340be3e720b78535fac5a05d9de79
Logan Barnes <logan@rotor.ai>
2025-03-31 14:26:49 -0600

    Add prettier for formatting

--- src/AppOne.ts
+++ src/AppOne.ts
@@ -1,15 +1,14 @@
-import * as BABYLON from 'babylonjs'
+import * as BABYLON from "babylonjs";
 export class AppOne {
     engine: BABYLON.Engine;
     scene: BABYLON.Scene;
 
     constructor(readonly canvas: HTMLCanvasElement) {
-        this.engine = new BABYLON.Engine(canvas)
-        window.addEventListener('resize', () => {
+        this.engine = new BABYLON.Engine(canvas);
+        window.addEventListener("resize", () => {
             this.engine.resize();
         });
-        this.scene = createScene(this.engine, this.canvas)
-
+        this.scene = createScene(this.engine, this.canvas);
     }
 
     debug(debugOn: boolean = true) {
@@ -26,20 +25,23 @@ export class AppOne {
             this.scene.render();
         });
     }
-
-
-
 }
 
-
-var createScene = function (engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
+function createScene(engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
     // this is the default code from the playground:
 
     // This creates a basic Babylon Scene object (non-mesh)
     var scene = new BABYLON.Scene(engine);
 
     // This creates and positions a free camera (non-mesh)
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+    var camera = new BABYLON.ArcRotateCamera(
+        "Camera",
+        0,
+        0,
+        10,
+        new BABYLON.Vector3(0, 0, 0),
+        scene,
+    );
 
     // This targets the camera to scene origin
     camera.setTarget(BABYLON.Vector3.Zero());
@@ -48,25 +50,36 @@ var createScene = function (engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
     camera.attachControl(canvas, true);
 
     // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+    var light = new BABYLON.HemisphericLight(
+        "light",
+        new BABYLON.Vector3(0, 1, 0),
+        scene,
+    );
 
     // Default intensity is 1. Let's dim the light a small amount
     light.intensity = 0.7;
 
     // Our built-in 'sphere' shape.
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
+    var sphere = BABYLON.MeshBuilder.CreateSphere(
+        "sphere",
+        { diameter: 2, segments: 32 },
+        scene,
+    );
     // Move the sphere upward 1/2 its height
     let startPos = 2;
     sphere.position.y = startPos;
 
     // Our built-in 'ground' shape.
-    var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
+    var ground = BABYLON.MeshBuilder.CreateGround(
+        "ground",
+        { width: 6, height: 6 },
+        scene,
+    );
     var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
     groundMaterial.diffuseColor = new BABYLON.Color3(0.5, 0.8, 0.5); // RGB for a greenish color
     ground.material = groundMaterial;
     groundMaterial.bumpTexture = new BABYLON.Texture("./normal.jpg", scene);
-    //groundMaterial.bumpTexture.level = 0.125;    
-
+    //groundMaterial.bumpTexture.level = 0.125;
 
     var redMaterial = new BABYLON.StandardMaterial("redMaterial", scene);
     redMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0); // RGB for red
@@ -79,7 +92,7 @@ var createScene = function (engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
     scene.registerBeforeRender(() => {
         sphereVelocity += gravity;
         let newY = sphere.position.y - sphereVelocity;
-        sphere.position.y -= sphereVelocity
+        sphere.position.y -= sphereVelocity;
         if (newY < 1) {
             sphereVelocity = (reboundLoss - 1) * sphereVelocity;
             newY = 1;
@@ -91,4 +104,4 @@ var createScene = function (engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
     });
 
     return scene;
-};
+}
